01f2ec9ea4ba3ffa4cd4bd371b6f663bf144e839
seoyun <newseoyun@gmail.com>
2021-02-16 22:56:07 +0900

    .

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,27 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team") // 연관관계를 맺었을 때, 외래키가 없는 곳에 mappedBy 적어준다)
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
