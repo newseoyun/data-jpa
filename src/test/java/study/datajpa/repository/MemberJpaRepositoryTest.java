56a936197ad48d90201a7f92817e0d16c2fbcf3c
seoyun <newseoyun@gmail.com>
2021-02-20 07:46:27 +0900

    .

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -80,4 +80,16 @@ class MemberJpaRepositoryTest {
 
     }
 
+    @Test
+    public void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+
+        List<Member> result = memberJpaRepository.findByUsername("AAA");
+        assertThat(result.size()).isEqualTo(1);
+    }
+
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -86,5 +86,16 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
 }
 
