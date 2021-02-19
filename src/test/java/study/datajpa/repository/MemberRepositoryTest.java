9adff513c51d02b5dba4455e0edaed7aa6a255a6
seoyun <newseoyun@gmail.com>
2021-02-19 21:59:50 +0900

    .

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -64,4 +64,27 @@ class MemberRepositoryTest {
         Assertions.assertThat(deleteCount).isEqualTo(0);
     }
 
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
+        Assertions.assertThat(result.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void findTop3HelloBy() {
+        List<Member> helloBy = memberRepository.findTop3HelloBy();
+
+    }
+
 }
+
