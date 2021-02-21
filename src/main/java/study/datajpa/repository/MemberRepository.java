f49c3bb0a39c2017712b8c3bb778e0a470cbf705
seoyun <newseoyun@gmail.com>
2021-02-21 23:55:22 +0900

    .

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -12,6 +12,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String name, int age);
     List<Member> findTop3HelloBy();
 
-    @Query(name = "Member.findByUsername")
+    //@Query(name = "Member.findByUsername") 이름이 기본 형식에 맞아서 여길 생략해도 잘 되긴 함.
     List<Member> findByUsername(@Param("username") String username);
+
+    // 실무에서 많이 사용하는 방식
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
