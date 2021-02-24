0184f8577372169a4bbc6e301caa6af0621b8100
seoyun <newseoyun@gmail.com>
2021-02-24 23:55:08 +0900

    .

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -12,6 +12,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -162,6 +163,33 @@ class MemberRepositoryTest {
         for (Member member : usernameList) {
             System.out.println("member = " + member);
         }
+    }
+
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // List<Member> result = memberRepository.findListByUsername("AAA");
+        // Member findMember = memberRepository.findMemberByUsername("AAA");
+        // Optional<Member> opMember = memberRepository.findOptionalByUsername("AAA");
+
+
+        // null이 아니라 빈 컬렉션 반환
+        List<Member> result = memberRepository.findListByUsername("asdf");
+        System.out.println("result = " + result.size());
+
+
+        // NULL. 자바 8부턴 Optional로 컨트롤하면 됨.
+        Member findMember = memberRepository.findMemberByUsername("asdf");
+        System.out.println("findMember = " + findMember);
+
+
+
     }
 
 
